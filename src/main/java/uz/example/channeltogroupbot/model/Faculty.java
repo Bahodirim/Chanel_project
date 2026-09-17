436b6149a141ec13ce867cb8ab51871e29b4738e
macstore <bakhodir571uz@gmail.com>
2026-09-17 22:36:12 +0500

    changed

--- src/main/java/uz/example/channeltogroupbot/model/Faculty.java
+++ src/main/java/uz/example/channeltogroupbot/model/Faculty.java
@@ -14,7 +14,7 @@ import java.util.Optional;
  */
 public enum Faculty {
 
-
+// changed
     MATEMATIKA(1, "Matematika", "matematika"),
     AMALIY_MATEMATIKA(2, "Amaliy matematika va intelektual texnologiyalar", "amit"),
     FIZIKA(3, "Fizika", "fizika"),
